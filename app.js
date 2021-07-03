5275d54bd274b775df5f62b0d27296802b44eb53
DarielST <86852459+DarielST@users.noreply.github.com>
2021-07-03 16:27:53 -0400

    Add files via upload
    inicio

---
+++ app.js
@@ -0,0 +1,18 @@
+///console.log("Hola mundo");
+
+
+const express = require("express");
+const app = express();
+const request = require("request");
+const async = require("async");
+
+
+app.get('/upcoming', (request, response)=>{
+    response.json({      
+        'Acesso': yes,
+});
+});
+
+app.listen("8010",()=>{
+    console.log("Servidor")
+})
